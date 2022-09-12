14e62706d774694afa6e06d2a3a6269b82e1ad80
gummy-b <thadhinsky@gmail.com>
2022-09-12 16:03:02 -0700

    Hot fix - Betty compliance

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
 * main - Entry Point
-* Description: This program will assign a random number 
+* Description: This program will assign a random number
 * to the variable n each time it is executed.
 * Return: Always 0
 */
@@ -14,7 +14,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is %d", n, n%10);
+printf("Last digit of %d is %d ", n, n % 10);
 if (n % 10 > 5)
 {
 printf("and is greater than 5\n");
@@ -23,7 +23,7 @@ else if (n % 10 == 0)
 {
 printf("and is 0\n");
 }
- else if (n % 10 < 6 && n % 10 != 0)
+else if (n % 10 < 6 && n % 10 != 0)
 {
 printf("and is less than 6 and not 0\n");
 }
