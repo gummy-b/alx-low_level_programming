3f29bcafd0feee5be063d41ce727eec80515d7c8
gummy-b <thadhinsky@gmail.com>
2022-09-12 14:37:25 -0700

    Hot fix - Betty compliance

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,26 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/*main - Program Entrance
+* description - Program assigns random number and checks 
+* whether number is positive, negative or equal zero
+*/
+
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if(n < 0){
-	printf("%d is negative\n", n);
-	}else if(n == 0){
-	  printf("%d is zero\n", n);
-	}
-	else
-	{
-	printf("%d is positive\n", n);
-	}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if(n < 0){
+printf("%d is negative\n", n);
+}else if(n == 0){
+  printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is positive\n", n);
+}
+return (0);
 }
