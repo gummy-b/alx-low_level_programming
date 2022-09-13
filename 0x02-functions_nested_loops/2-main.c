9db4124de52bc67312349e4677c322bec18a0119
gummy-b <thadhinsky@gmail.com>
2022-09-13 15:23:34 -0700

    a program that prints the alphabets x10

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* main - entry point for print alphabet x10
+* Return: Always 0.
+*/
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Prints alphabets x10, nested loops
+* Return: Always 0.
+*/
+
+void print_alphabet_x10(void)
+{
+int nl = 0; /* new line */
+while(nl < 10)
+{
+char al = 'a'; /* starting alphabet to loop from */
+while (al <= 'z')
+{
+_putchar(al);
+al++;
+}
+_putchar('\n');
+nl++;
+}
+}
