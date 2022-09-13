9966efe1dd41e5d94e99d2287e2e7cd007576a21
gummy-b <thadhinsky@gmail.com>
2022-09-13 14:12:31 -0700

    a program that prints _putchar and new line

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
 * main: Entry Point
 * Descirption: prints char c to standard output
 * Return: Always 0
 */
-int _putchar(char c);
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
