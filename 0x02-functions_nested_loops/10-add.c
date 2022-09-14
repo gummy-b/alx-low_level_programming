cbf6f254ab8ea6ed38cbdb9720b1a2146f74a862
gummy-b <thadhinsky@gmail.com>
2022-09-14 16:14:07 -0700

    a program that sums up two int numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* add - function adds two integers
+* @a, @b: First and second input of int to be added.
+* Return: sum of @a and @b.
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
