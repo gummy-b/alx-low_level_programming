245c556645d87bb68f760f0b1051d1b5d465d375
gummy-b <thadhinsky@gmail.com>
2022-09-14 14:58:22 -0700

    A function that prints out the absolute value of int input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - function computes the absolute value of an int.
+* @int: any
+* Return: Always 0
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
