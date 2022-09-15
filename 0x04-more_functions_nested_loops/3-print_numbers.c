9d71c47636de431562c1ff1a3a143bd9c33d7d45
gummy-b <thadhinsky@gmail.com>
2022-09-15 15:14:55 -0700

    Betty compliance - Adjust indentation

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,10 @@
 */
 void print_numbers(void)
 {
-  int n;
-  for (n = 48; n < 58; n++)
-    {
-      _putchar(n);
-    }
-  _putchar(10);
+int n;
+for (n = 48; n < 58; n++)
+{
+_putchar(n);
+}
+_putchar(10);
 }
