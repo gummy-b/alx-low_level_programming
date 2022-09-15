05b6858d40e5f9fa505dbb3e6f71fdad31d99c46
gummy-b <thadhinsky@gmail.com>
2022-09-15 13:35:31 -0700

    add function tht checks whether input is a digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - checks to see whether a char is a digit or not
+* @c: char input to be tested.
+* Return: 1 if c is a digit, otherwise 0.
+*/
+int _isdigit(int c)
+{
+  if((c >= 48) && (c <= 57))
+    {
+      return (1);
+    }
+  return (0);
+}
