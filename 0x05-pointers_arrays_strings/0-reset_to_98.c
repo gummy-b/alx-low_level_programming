7c223bb3e6bdc4c0cf99673c162f4fded4938f58
gummy-b <thadhinsky@gmail.com>
2022-09-19 12:53:56 -0700

    add param description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 * reset_to_98 - update number using pointer
 * Description: a function that takes a pointer
 * to an int as param and updates value to point to 98.
+* @n: Takes in a pointer param
 * Return: void
 */
 void reset_to_98(int *n)
